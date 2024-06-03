c9aad25f8991c05fbc2d85b5e23e59d1e2266b03
rubenreh <149988511+rubenreh@users.noreply.github.com>
2024-06-02 23:39:00 -0400

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,11 @@
   <main
     class="mt-10 md:mt-1 flex flex-col-reverse gap-8 items-center md:flex-row md:gap-16 md:justify-center min-h-[65vh] md:min-h-[80vh]">
     <div class="space-y-2 text-center md:text-left px-10">
-      <p class="text-green-200">Hey there, I'm</p>
+      <p class="text-orange-200">Hey there, I'm</p>
       <h1 class="text-4xl font-bold md:text-5xl text-white fadein-up">Ruben Rehal</h1>
       <div class="py-2">
         <h1
-          class="typewrite text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-green-500 md:text-2xl fadein-up"
+          class="typewrite text-xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-orange-500 md:text-2xl fadein-up"
           ref="typewriter">
           <span class="wrap">{{ txt }}</span>
         </h1>
