b8227ff8a6d9c819b0b909d156d7895987880c0f
rubenreh <149988511+rubenreh@users.noreply.github.com>
2024-06-02 22:10:30 -0400

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -24,7 +24,7 @@
           </path>
         </svg>Download Resume</button> -->
     </div>
-    <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="100" height="100" decoding="async" data-nimg="1"
+    <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
         class="w-10/12 md:h-auto rounded-full border-4 border-amber-200 pict" src="https://github.com/rubenreh/Ruben-Rehal-Portfolio/blob/main/public/img/StandingStage.PNG?raw=true">
     </div>
   </main>
