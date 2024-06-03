4a82657fdac13515dfbb4ac32d5c638532ec9ad2
rubenreh <149988511+rubenreh@users.noreply.github.com>
2024-06-02 23:03:52 -0400

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <main
     class="mt-10 md:mt-1 flex flex-col-reverse gap-8 items-center md:flex-row md:gap-16 md:justify-center min-h-[65vh] md:min-h-[80vh]">
     <div class="space-y-2 text-center md:text-left px-10">
-      <p class="text-blue-200">Hello there, I'm</p>
+      <p class="text-green-200">Hello there, I'm</p>
       <h1 class="text-4xl font-bold md:text-5xl text-white fadein-up">Ruben Rehal</h1>
       <div class="py-2">
         <h1
