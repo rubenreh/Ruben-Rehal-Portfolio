259f9395eaa180391b6b6214fc040d3d1192b8b5
rubenreh <149988511+rubenreh@users.noreply.github.com>
2024-06-03 16:29:23 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ ts<template>
           <span class="self-center text-lg text-[#DCA052] font-semibold whitespace-nowrap fadein-bot hover:text-white-100">Ruben Rehal</span>
         </button>
         <div class="flex md:order-2 fadein-bot">
-          <a href="https://github.com/rubenreh"><img class="w-9 rounded-full" src="https://seeklogo.com/images/G/github-logo-2E3852456C-seeklogo.com.png" alt="github"></a>
+          <a href="https://github.com/rubenrehal"><img class="w-9 rounded-full" src="https://seeklogo.com/images/G/github-logo-2E3852456C-seeklogo.com.png" alt="github"></a>
         </div>
         <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
           <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
