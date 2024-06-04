0c2148b419062e6079438278d3bb00c38904f1f3
rubenreh <149988511+rubenreh@users.noreply.github.com>
2024-06-04 13:11:55 -0400

    Update BlogView.vue

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -64,7 +64,7 @@ export default {
   },
   methods: {
     async getArticles() {
-      axios.get('https://64a38c9cc3b509573b564183.mockapi.io/api/blog/all')
+      axios.get('https://665f47721e9017dc16f39a02.mockapi.io/api/blog/update1')
         .then(response => {
           this.articles = response.data;
         })
