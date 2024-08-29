1bebdf17a625367350d9b3790d0dba6ff3464396
Ruben Rehal <149988511+rubenreh@users.noreply.github.com>
2024-08-29 10:12:23 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ ts<template>
             </li>
             <li>
               <router-link to="/blog"
-                class="fadein-bot fadein-3 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0 blog">Blog</router-link>
+                class="fadein-bot fadein-3 text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-gray-500 md:p-0 blog">Updates</router-link>
             </li>
           </ul>
         </div>
